123a85d4c938412c18ebc9eb33cdb9bfe3ef9e15
Garba jamiu <garbajamiu2000@gmail.com>
2024-08-07 14:43:52 +0100

    initial commit

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.0;
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
+
+    event TransferFrom(address from, address to);
 }
 
 contract Escrow {
-
+    address public nftAddress;
+    address public inspector;
+    address public lender;
+    address payable public seller;
 }
